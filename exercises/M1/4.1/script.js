67cff7592a12d060275044bab9a1719f338f430e
brenno-calado <brenno.calado@gmail.com>
2020-11-25 18:35:46 -0300

    Task 2

--- exercises/M1/4.1/script.js
+++ exercises/M1/4.1/script.js
@@ -1,8 +1,18 @@
 var a = 10;
 var b = 5;
 
+//Task 1
+
 let add = a + b
 let subtract = a - b;
 let multiply = a * b; 
 let divide = a / b;
 let remainder = a % b;
+
+//Task 2
+
+let greaterThan = a > b ? a : b;
+
+//Output area
+
+console.log(add);
