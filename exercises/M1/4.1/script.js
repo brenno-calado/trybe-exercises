a1ad869f0f5d496fce44bef39a4170de8a71a986
brenno-calado <brenno.calado@gmail.com>
2020-11-25 19:04:51 -0300

    Task 3

--- exercises/M1/4.1/script.js
+++ exercises/M1/4.1/script.js
@@ -1,5 +1,6 @@
 var a = 10;
 var b = 5;
+var c = 2;
 
 //Task 1
 
@@ -13,6 +14,21 @@ let remainder = a % b;
 
 let greaterThan = a > b ? a : b;
 
+//Task 3
+
+function greaterOfThree(a,b,c) {
+    if (a > b && a > c) {
+        console.log(a)
+    }
+    else if (b > c) {
+        console.log(b)
+    }
+    else if (c != a && c != b) {
+        console.log(c)
+    }
+    else {console.log('Os números são iguais')}
+};
+
 //Output area
 
-console.log(add);
+console.log(greaterOfThree(a,b,c));
