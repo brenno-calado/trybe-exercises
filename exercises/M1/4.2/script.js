797ae578d1b2b53ed3e21d61ed969d34cf76a93a
brenno-calado <brenno.calado@gmail.com>
2020-11-26 17:51:21 -0300

    Task 1

---
+++ exercises/M1/4.2/script.js
@@ -0,0 +1,46 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let index = null
+
+// Task 1
+
+function countArray(numbers) {
+for (index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
+};
+};
+
+//Task 2
+
+
+
+//Task 3
+
+
+
+//Task 4
+
+
+
+//Task 5
+
+
+
+//Task 6
+
+
+
+//Task 7
+
+
+
+//Task 8
+
+
+
+//Task 9
+
+
+
+//Output area
+
+return countArray(numbers);
