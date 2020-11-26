72fe9e421b95dd0d1dc69aa5795d0b383b5d4bb0
brenno-calado <brenno.calado@gmail.com>
2020-11-25 23:54:52 -0300

    Task 11

--- exercises/M1/4.1/script.js
+++ exercises/M1/4.1/script.js
@@ -123,6 +123,46 @@ function grandProfit(a,c) {
     };
 };
 
+//Task 11
+
+var salarioBruto = 3500;
+function INSS_IR(salarioBruto) {
+    let inss;
+    let salarioBase;
+    let ir;
+    let salarioLiquido;
+
+    if (salarioBruto <= 1556.94 && salarioBruto >= 0) {
+        inss = salarioBruto * 0.08;
+    } else if (salarioBruto >=1556.95 && salarioBruto <= 2594.92) {
+        inss = salarioBruto * 0.09;
+    } else if (salarioBruto >=2594.93 && salarioBruto <= 5189.82) {
+        inss = salarioBruto * 0.11;
+    } else if (salarioBruto > 5189.82) {
+        inss = salarioBruto + 570.88;
+    } else {
+        console.log('Valor precisa ser maior que 0');
+    };
+
+    salarioBase = salarioBruto - inss;
+
+    if (salarioBase <= 1903.98 && salarioBase >= 0) {
+        ir = 0;
+    } else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+        ir = [salarioBase * 0.075] - 142.8;
+    } else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+        ir = [salarioBase * 0.15 ] - 354.8;
+    } else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+        ir = [salarioBase * 0.225] - 636.13;
+    } else if (salarioBase > 4664.68) {
+        ir = [salarioBase * 0.275] - 869.36;
+    } else {
+        console.log('Valor precisa ser maior que 0');
+    };
+    salarioLiquido = salarioBase - ir;
+    console.log(salarioLiquido);
+};
+
 //Output area
 
-return grandProfit(a,c);
+return INSS_IR(salarioBruto);
