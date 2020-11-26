152f1e5cd38c6472c570947d66ce3c4489fab829
brenno-calado <brenno.calado@gmail.com>
2020-11-26 18:07:08 -0300

    Task 2

--- exercises/M1/4.2/script.js
+++ exercises/M1/4.2/script.js
@@ -1,17 +1,23 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let index = null
+let index = 0;
+let sumNumbers = 0;
 
 // Task 1
 
 function countArray(numbers) {
-for (index = 0; index < numbers.length; index += 1) {
+  for (index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
-};
+  };
 };
 
 //Task 2
 
-
+function sumArray(numbers) {
+  for (index = 0; index < numbers.length; index += 1) {
+    sumNumbers += numbers[index];
+  };
+  console.log(sumNumbers);
+};
 
 //Task 3
 
@@ -41,6 +47,3 @@ for (index = 0; index < numbers.length; index += 1) {
 
 
 
-//Output area
-
-return countArray(numbers);
