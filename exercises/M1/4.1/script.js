65dd98976f281d3b0eb9444835b3ffc291f48ad1
brenno-calado <brenno.calado@gmail.com>
2020-11-25 22:33:03 -0300

    Task 7 & Update Task 6

--- exercises/M1/4.1/script.js
+++ exercises/M1/4.1/script.js
@@ -51,34 +51,49 @@ function chessMoves(chessman) {
     switch(chessMove) {
         case 'pawn':
         chessMove = 'Advance one square along the same file. Advance two unoccupied squares at the start position. Capture at one diagonally forward square.';
-        console.log(chessMove);
         break;
         case 'rook':
         chessMove = 'Advance all unoccupied squares along a rank or file. Can do Castling';
-        console.log(chessMove);
         break;
         case 'knight':
         chessMove = 'Moves to the closest square which is not on the same file, rank or diagonal. Can leap chessman.';
-        console.log(chessMove);
         break;
         case 'bishop':
         chessMove = 'Moves to all unoccupied diagonal squares';
-        console.log(chessMove);
         break;
         case 'queen':
         chessMove = 'Moves to all unoccupied files, ranks and diagonal squares';
-        console.log(chessMove);
         break;
         case 'king':
         chessMove = 'Moves to all unoccupied adjacent squares. Can do Castling';
-        console.log(chessMove);
         break;
         default:
         chessMove = "Unkown chessman. Try again.";
-        console.log(chessMove);
-    }
+    };
+    console.log(chessMove);
+};
+
+//Task 7
+
+var points = 600;
+function pointToGrade(points) {
+    let grade = null;
+    if (points >= 90 && points <= 100) {
+        grade = 'A';
+    } else if (points >= 80 && points < 90) {
+        grade = 'B';
+    } else if (points >= 70 && points < 80) {
+        grade = 'C';
+    } else if (points >= 60 && points < 70) {
+        grade = 'D';
+    } else if (points >= 50 && points < 60) {
+        grade = 'E';
+    } else if (points >= 0 && points < 50) {
+        grade = 'F';
+    } else {console.log('points must range from 0 to 100')};
+    console.log('Your grade is ' + grade);
 };
 
 //Output area
 
-return chessMoves(chessman);
+return pointToGrade(points);
