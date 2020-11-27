ea5e63c03f00a802e08279b65a75b0d9677f2e0f
brenno-calado <brenno.calado@gmail.com>
2020-11-27 18:23:23 -0300

    Task 2

--- exercises/M1/4.3/script.js
+++ exercises/M1/4.3/script.js
@@ -1,7 +1,9 @@
-let n = 1.1;
+let n = 3;
 let index = 0;
 let dot = '';
 let indexSecond = 0;
+
+//Task 1
 
 if (n > 1) {
   Math.ceil(n);
@@ -14,3 +16,18 @@ if (n > 1) {
 } else {
   console.log('Número precisa ser maior que 1');
 };
+console.log('\n')
+
+//Task 2
+
+dot = '';
+if (n > 1) {
+  Math.ceil(n);
+  for (indexSecond = 0; indexSecond < n; indexSecond += 1) {
+  dot += '*';
+  console.log(dot);
+  };
+} else {
+  console.log('Número precisa ser maior que 1');
+};
+
