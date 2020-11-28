ba10d4e24fcfce4937da47151ae3f14aa6c031e6
brenno-calado <brenno.calado@gmail.com>
2020-11-28 01:51:53 -0300

    Task 4 OMG

--- exercises/M1/4.3/script.js
+++ exercises/M1/4.3/script.js
@@ -1,4 +1,4 @@
-let n = 6;
+let n = 7;
 let index = 0;
 let dot = '';
 let indexSecond = 0;
@@ -38,14 +38,52 @@ if (n > 1) {
 console.log('\nTask 3\nn = ',n);
 
 //Task 3
-//usar .repeat para adicionar string
-indexThird = 0;
+
 dot = '';
-  for (indexSecond = 0; indexSecond < n; indexSecond += 1) {
+  for (index = 0; index < n; index += 1) {
     dot += '*';
     blanks = '';
-    for (indexThird = 0; indexThird < (n - indexSecond) - 1; indexThird += 1) {
+    for (indexSecond = 0; indexSecond < (n - index) - 1; indexSecond += 1) {
     blanks += ' ';
     };
     console.log(blanks + dot);
   };
+
+
+console.log('\nTask 4\n')
+
+//Task 4
+//if n % 2 == 0
+// index of dot = 2 and moves 2 by 2
+//index of space = add half of n and roof number - index of dot
+//print (blanks + dot + blanks)
+dot = '';
+if (n >= 3) {
+  Math.round(n);
+  if (n % 2 == 0) {
+    for (index = 0; index < n; index += 2) {
+      dot += '**';
+      blanks = '';
+      for (indexSecond = 0; indexSecond < (n - index) - 2; indexSecond += 2) {
+          blanks += ' ';
+          };
+      console.log(blanks + dot + blanks);
+    };
+  } else {
+      for (index = 0; index < n; index += 2) {
+        if (index == 0) {
+          dot += '*';
+          blanks = '';
+        } else {
+        dot += '**';
+        blanks = '';
+        };
+        for (indexSecond = 1; indexSecond < n - index; indexSecond += 2) {
+          blanks += ' ';
+          };
+        console.log(blanks + dot + blanks);
+        };
+      };
+} else {
+  console.log('Number is not greater or equal to 2');
+};
