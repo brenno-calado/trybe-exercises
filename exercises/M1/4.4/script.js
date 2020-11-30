e3a05bfd3835283ffb6c08eac6f8fad623937d9e
brenno-calado <brenno.calado@gmail.com>
2020-11-30 15:33:11 -0300

    initial commit 4.4

---
+++ exercises/M1/4.4/script.js
@@ -0,0 +1,11 @@
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  },
+  bestInTheWorld: [2006, 2007,]
+}
+let ageMessage = console.log('@ Jogador@',player.name,player.lastName,'tem',player.age,'anos de idade');
