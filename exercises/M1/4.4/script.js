2ced03ccb3a9ff689bc566ae5b6ef119b6c26bf8
brenno-calado <brenno.calado@gmail.com>
2020-11-30 15:39:19 -0300

    Marta Silva object exercise

--- exercises/M1/4.4/script.js
+++ exercises/M1/4.4/script.js
@@ -6,6 +6,8 @@ let player = {
     golden: 2,
     silver: 3,
   },
-  bestInTheWorld: [2006, 2007,]
+  bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
 }
 let ageMessage = console.log('@ Jogador@',player.name,player.lastName,'tem',player.age,'anos de idade');
+let bestMessage = console.log('@ Jogadora',player.name,player.lastName,'foi eleita a melhor do mundo por',player.bestInTheWorld.length,'vezes');
+let medalsMessage = console.log('@ Jogador@ possui',player.medals.golden,'medalhas de ouro e',player.medals.silver,'medalhas de prata');
