4e85e9d907a996db34ca0f984e1915e98e982497
brenno-calado <brenno.calado@gmail.com>
2020-11-30 16:00:03 -0300

    for in

--- exercises/M1/4.4/script.js
+++ exercises/M1/4.4/script.js
@@ -11,3 +11,6 @@ let player = {
 let ageMessage = console.log('@ Jogador@',player.name,player.lastName,'tem',player.age,'anos de idade');
 let bestMessage = console.log('@ Jogadora',player.name,player.lastName,'foi eleita a melhor do mundo por',player.bestInTheWorld.length,'vezes');
 let medalsMessage = console.log('@ Jogador@ possui',player.medals.golden,'medalhas de ouro e',player.medals.silver,'medalhas de prata');
+for (let i in player) {
+  console.log(i, player[i]);
+}
