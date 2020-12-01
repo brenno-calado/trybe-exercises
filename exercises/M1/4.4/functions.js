1187e3c7455a9fe3c1bea09cb03a6d68b8ac6ed0
brenno-calado <brenno.calado@gmail.com>
2020-12-01 13:46:15 -0300

    Part II Task 7 incomplete

--- exercises/M1/4.4/functions.js
+++ exercises/M1/4.4/functions.js
@@ -5,67 +5,63 @@ function verifypalindrome(word) {
   for (let index = word.length - 1; index >= 0; index -= 1) {
     backwardsWord += word[index];
   };
-  console.log(backwardsWord);
   let palindromeCheck = backwardsWord === word;
-  console.log(palindromeCheck);
-}
+  return palindromeCheck;
+};
 
 function greaterFromArray(arrayOne) {
-  let theGreatest = 0;
   let greatestIndex = 0;
-  for (let index = 0; index < arrayOne.length; index += 1) {
-    if (theGreatest < arrayOne[index]) {
-      theGreatest = arrayOne[index];
+  for (let index in arrayOne) {
+    if (arrayOne[greatestIndex] < arrayOne[index]) {
       greatestIndex = index;
-    } else continue;
+    };
   };
   return greatestIndex;
 };
-let testArray = [2, 3, 6, 7, 10, 11];
-console.log(greaterFromArray(testArray));
+
+console.log(greaterFromArray([2, 3, 6, 7, 10, 11]));
 
 function smallerFromArray(arrayOne) {
-  let theSmallest = arrayOne[0];
   let smallestIndex = arrayOne[0];
-  for (let index = 0; index < arrayOne.length; index += 1) {
-    if (theSmallest >= arrayOne[index]) {
-      theSmallest = arrayOne[index];
+  for (let index in arrayOne) {
+    if (arrayOne[smallestIndex] > arrayOne[index]) {
       smallestIndex = index;
-    } else continue;
+    };
   };
   return smallestIndex;
 };
-console.log(smallerFromArray(testArray));
+console.log(smallerFromArray([2, 3, 6, 7, 10, 11]));
 
 function bigName(nameList) {
   let biggestName = nameList[0];
-  for (let index = 0; index < nameList.length; index += 1) {
+  for (let index in nameList) {
     if (biggestName.length < nameList[index].length) {
       biggestName = nameList[index];
-    } else continue;
+    };
   };
   return biggestName;
 };
-nameList = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
-console.log(bigName(nameList));
+
+console.log(bigName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
 
 function modeNumber(numberList) {
+  let index = 0;
   let indexCompare = 0;
   let mode = 0;
   let bestMode = 0;
   let bestModeIndex = 0;
-  for (let index = 0; index < numberList.length; index += 1) {
+  for (index in numberList) {
     mode = 0;
-    for (indexCompare = 0; indexCompare < numberList.length; indexCompare += 1) {
+    for (indexCompare in numberList) {
       mode = numberList[index] == numberList[indexCompare] ? mode += 1 : mode = mode;
     };
     if (bestMode < mode) {
       bestMode = mode;
       bestModeIndex = index;
-    } else continue;
     };
-    return numberList[bestModeIndex];
   };
+    return numberList[bestModeIndex];
+};
 
 let numberList = [2, 3, 2, 5, 8, 2, 3];
 console.log(modeNumber(numberList));
@@ -80,3 +76,19 @@ function sumTillNumber(number) {
   return sumResult;
 };
 console.log(sumTillNumber(number));
+
+//Used Trybe feedback for exercise 7
+
+function verifyEndWord(word,ending) {
+  let verifyCheck = true;
+  if (word.length >= ending.length) {
+    for (let index in ending) {
+      if (word[(word.length - ending.length) + index] != ending[index]) {
+        verifyCheck = false;
+      };      
+    };
+    return verifyCheck;
+  } else {console.log('ending word must be smaller than comparing word')};
+};
+
+console.log(verifyEndWord('brenno','on'));
