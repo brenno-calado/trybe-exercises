01585b3bab41d1b57de0ce77202312e3279145fd
brenno-calado <brenno.calado@gmail.com>
2021-01-05 10:50:31 -0300

    deleted svg files, animated SVG with JS, darkmode preference can be saved in localStorage

--- exercises/M1/5.4/script.js
+++ exercises/M1/5.4/script.js
@@ -4,16 +4,55 @@
 // Espaçamento entre as linhas do texto;
 // Tipo da fonte ( Font family ).
 
-let darkMode = document.getElementById('darkMode');
-darkMode.addEventListener('click', toDarkMode);
-let bg = document.querySelector('body');
-let isDarkMode = false;
-let meuStorage = localStorage;
-
-function toDarkMode(isDarkMode) {
-  if (isDarkMode) {
-  bg.style.backgroundColor = '#242424';
-  bg.style.color = '#d8d8d8';
-  meuStorage.setItem(isDarkMode, true);
+function toDarkMode() {
+  let outsideMoon = document.querySelector('#outside-moon');
+  let moon = document.querySelector('#moon');
+  let body = document.querySelector('body');
+  let meuStorage = localStorage;
+  let savedMode = meuStorage.getItem('isDarkMode');
+  if (savedMode === 'true') {
+    body.classList.remove('dark-mode');
+    meuStorage.removeItem('isDarkMode');
+    meuStorage.setItem('isDarkMode', false);
+    outsideMoon.classList.remove('light');
+    outsideMoon.classList.add('dark');
+    moon.classList.remove('dark');
+    moon.classList.add('light');
+  } else {
+    body.classList.add('dark-mode');
+    meuStorage.removeItem('isDarkMode');
+    meuStorage.setItem('isDarkMode', true);
+    outsideMoon.classList.remove('dark');
+    outsideMoon.classList.add('light');
+    moon.classList.remove('light');
+    moon.classList.add('dark');
   }
 }
+
+function darkModeFunction() {
+  let darkModeButton = document.getElementById('dark-mode-button');
+  darkModeButton.addEventListener('click', toDarkMode);
+}
+
+function checkSavedMode() {
+  let outsideMoon = document.querySelector('#outside-moon');
+  let moon = document.querySelector('#moon');
+  let body = document.querySelector('body');
+  let meuStorage = localStorage;
+  let savedMode = meuStorage.getItem('isDarkMode');
+  console.log(savedMode);
+  if (savedMode === 'true') {
+    body.classList.add('dark-mode');
+    outsideMoon.classList.remove('dark');
+    outsideMoon.classList.add('light');
+    moon.classList.remove('light');
+    moon.classList.add('dark');
+  } else {
+    body.classList.remove('dark-mode');
+  }
+}
+
+window.onload = function () {
+  checkSavedMode();
+  darkModeFunction();
+};
