d4d195f807a6ed98ea5d8078da269fdc8600220d
brenno-calado <brenno.calado@gmail.com>
2021-01-12 18:38:08 -0300

    segundo exercicio usando sort

--- exercises/M1/7.1/script.js
+++ exercises/M1/7.1/script.js
@@ -1,12 +1,9 @@
-const testingScope = escopo => { 
-  if (escopo) { 
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if).';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const orderArray = oddsAndEvens => {
+  oddsAndEvens.sort(function(a, b){return a - b});
 }
 
-testingScope(false);
+orderArray(oddsAndEvens);
+
+console.log(oddsAndEvens);
