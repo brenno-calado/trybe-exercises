bbc4b03bd0d95068851c251ab8ed4638b9a3cf94
brenno-calado <brenno.calado@gmail.com>
2021-01-13 22:24:40 -0300

    exercise II.1 & II.2

---
+++ exercises/M1/7.2/exercise2.js
@@ -0,0 +1,27 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+const turno = 'turno';
+const manha = 'manha';
+
+const changeSchedule = (object, key, value) => object[key] = value;
+changeSchedule(lesson2, turno, manha);
+
+const listKeys = object => Object.keys(object);
+console.log(listKeys(lesson3));
