beae8112a881ef44a40d5c0d4fe69ba1e96530c9
brenno-calado <brenno.calado@gmail.com>
2021-01-14 18:28:33 -0300

    test myRemove stays the same

--- exercises/M1/7.3/ex2.js
+++ exercises/M1/7.3/ex2.js
@@ -14,3 +14,4 @@ function myRemove(arr, item) {
 
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
 assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
