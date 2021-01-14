058725ee9a8ece0f38c925b262b92db92e94f8a4
brenno-calado <brenno.calado@gmail.com>
2021-01-14 09:47:07 -0300

    Exercise II.6

--- exercises/M1/7.2/exercise2.js
+++ exercises/M1/7.2/exercise2.js
@@ -26,10 +26,14 @@ changeSchedule(lesson2, turno, manha);
 const listKeys = object => Object.keys(object);
 const objectSize = object => listKeys(object).length;
 const listValues = object => Object.values(object);
-const lessonList = {
-  lesson1,
-  lesson2,
-  lesson3
-}
-const allLessons = () => Object.assign({}, lessonList);
-console.log(allLessons());
+const allLessons = {};
+const mergeLessons = () => Object.assign(allLessons, {lesson1,lesson2,lesson3});
+mergeLessons();
+const sumStudents = () => {
+  let sum = 0;
+  for (let i in allLessons) {
+    sum += allLessons[i].numeroEstudantes
+  }
+  return sum;
+}; 
+console.log(sumStudents());
