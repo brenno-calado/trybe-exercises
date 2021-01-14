faed3e212331d9a7ef4c8b52d31224eadb8d79f3
brenno-calado <brenno.calado@gmail.com>
2021-01-14 18:17:57 -0300

    test string used &  error message

--- exercises/M1/7.3/ex1.js
+++ exercises/M1/7.3/ex1.js
@@ -12,3 +12,5 @@ function sum(a, b) {
 
 assert.strictEqual(sum(4, 5), 9);
 assert.strictEqual(sum(0, 0), 0);
+assert.throws(() => sum(4, '5'));
+assert.throws(() => sum(4, '5'), /^Error: parameters must be numbers$/);
