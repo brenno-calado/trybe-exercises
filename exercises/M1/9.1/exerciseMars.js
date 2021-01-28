c465c7be602fc4420db22aa5e8c58147885da0c6
brenno-calado <brenno.calado@gmail.com>
2021-01-27 21:17:48 -0300

    exercise Mars complete

--- exercises/M1/9.1/exerciseMars.js
+++ exercises/M1/9.1/exerciseMars.js
@@ -11,7 +11,7 @@
 
 // getPlanet(); // imprime Marte depois de 4 segundos
 
-const messageDelay = () => Math.floor(Math.random() * 5000);
+const messageDelay = () => Math.floor(Math.random() * 500);
 
 const getMarsTemperature = () => {
   const maxTemperature = 58;
@@ -21,17 +21,24 @@ const getMarsTemperature = () => {
 const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
 const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
 
-const sendMarsTemperature = (callback) => {
-  const temperature = getMarsTemperature();
-  const message = `Mars temperature is: ${temperature} degree Celsius`;
+const sendMarsTemperature = (callback, error) => {
   const timeDelay = messageDelay();
   setTimeout(() => {
-    console.log(message);
-    setTimeout(() => callback(temperature), timeDelay);
-  }, timeDelay);
+    const temperature = getMarsTemperature();
+    const message = `Mars temperature is: ${temperature} degree Celsius`;
+    const attempt = Math.random() >= 0.5;
+    if(attempt) {
+      error('Robot is busy');
+    }
+    else {
+      console.log(message);
+      setTimeout(() => callback(temperature), timeDelay);  
+    }
+    }, timeDelay);
 }
 
 // sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
-sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
-sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+sendMarsTemperature(temperatureInFahrenheit, handleError); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet, handleError); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
