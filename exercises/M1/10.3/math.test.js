d10d316d36217e4b0b5215d49f4ff61f4b23f7fc
brenno-calado <brenno.calado@gmail.com>
2021-02-10 15:00:27 -0300

    math.test.js

--- exercises/M1/10.3/math.test.js
+++ exercises/M1/10.3/math.test.js
@@ -1,23 +1,36 @@
 const math = require('./math');
 
-test("#somar", () => {
-  // testando a implementação original, o mock e a restauração da função original
+describe('testando funções da calculadora', () => {
+  test('#subtrair', () => {
+    // testando a implementação original, o mock e a restauração da função original
+  
+    // implementação original
+    expect(math.subtrair(2, 2)).toBe(0);
+  
+    // criando o mock e substituindo a implementação para uma subtração
+    const mockSubtrair = jest
+      .spyOn(math, 'subtrair')
+      .mockImplementation((a, b) => a - b);
+  
+    math.subtrair(5, 1);
+    expect(mockSubtrair).toHaveBeenCalledTimes(1);
+    expect(mockSubtrair(5, 1)).toBe(4);
+    expect(mockSubtrair).toHaveBeenCalledTimes(2);
+    expect(mockSubtrair).toHaveBeenLastCalledWith(5, 1);
+  
+    // restaurando a implementação original
+    math.subtrair.mockRestore();
+    expect(math.subtrair(3, 2)).toBe(1);
+  });
 
-  // implementação original
-  expect(math.somar(1, 2)).resolves.toBe(3);
-
-  // criando o mock e substituindo a implementação para uma subtração
-  const mockSomar = jest
-    .spyOn(math, "somar")
-    .mockImplementation((a, b) => a - b);
-
-  math.somar(5, 1);
-  expect(mockSomar).toHaveBeenCalledTimes(1);
-  expect(mockSomar(5, 1)).toBe(4);
-  expect(mockSomar).toHaveBeenCalledTimes(2);
-  expect(mockSomar).toHaveBeenLastCalledWith(5, 1);
-
-  // restaurando a implementação original
-  math.somar.mockRestore();
-  expect(math.somar(1, 2)).resolves.toBe(3);
+  test('#Multiplicar', () => {
+    const mockMultiplicar = jest
+      .spyOn(math, 'multiplicar')
+      .mockImplementation((a, b) => a * b)
+      .mockReturnValueOnce(10);
+    math.multiplicar(5, 3);
+    expect(mockMultiplicar(5, 1)).toBe(5);
+    expect(mockMultiplicar).toHaveBeenCalledTimes(2);
+    expect(mockMultiplicar).toHaveBeenCalledWith(5, 1);
+  })
 });
