5f0e367c85fabf9c334fc6554485fb22ce084941
brenno-calado <brenno.calado@gmail.com>
2021-02-10 19:31:34 -0300

    add tarefas

--- exercises/M2/11.1/first-react/src/App.js
+++ exercises/M2/11.1/first-react/src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import HelloWorld from './helloWorld';
 import MyName from './component';
 
+const taskList = ['arrumar casa', 'trabalhar', 'estudar', 'limpar carro'];
+
+const task = (list => list.map(value => <li>{value}</li>));
+
 function App() {
   return (
     <div className="App">
       <HelloWorld />
       <MyName />
+      <ul>{task(taskList)}</ul>
     </div>
   )
 }
