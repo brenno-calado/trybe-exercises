e955e165a188242b2be1b882888a17abeaed91a4
brenno-calado <brenno.calado@gmail.com>
2021-03-22 13:40:05 -0300

    renderWithRouter tests

--- exercises/M2/15.3/react-testing-library/src/App.jsx
+++ exercises/M2/15.3/react-testing-library/src/App.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+import NoMatch from './NoMatch';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route component={NoMatch} />
+        </Switch>
       </header>
     </div>
   );
 }
-
-export default App;
--- exercises/M2/15.3/react-testing-library/src/App.test.js
+++ exercises/M2/15.3/react-testing-library/src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import About from './About'
+import renderWithRouter from './renderWithRouter';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('App', () => {
+  renderWithRouter(<App />);
+  const home = screen.getByText('Homepage');
+  expect(home).toBeInTheDocument();
 });
+
+test('render About through click', () => {
+  const { history } = renderWithRouter(<App />);
+  fireEvent.click(screen.getByText(/About/i));
+  const pathname = history.location.pathname;
+  expect(pathname).toBe('/about');
+  expect(screen.getByText(/About page/)).toBeInTheDocument();
+});
+
+test('No match found', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/pagina-que-nao-existe');
+  expect(screen.getByText(/Page not found/)).toBeInTheDocument();
+});
+
+test('render About only', () => {
+  renderWithRouter(<About />);
+  expect(screen.getByText(/About page/)).toBeInTheDocument();
+})
