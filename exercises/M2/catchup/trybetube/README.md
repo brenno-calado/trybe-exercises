1a25d965cdd68dfbf702bdfb40b1b17f5967ac9d
brenno-calado <brenno.calado@gmail.com>
2021-04-05 15:23:29 -0300

    catchup trybetube

---
+++ exercises/M2/catchup/trybetube/README.md
@@ -0,0 +1,26 @@
+# Grupo2 - Parte1 #
+
+Essa seção contém os requisitos de um projeto para ser desenvolvido usando a base do React.
+
+Nessa Parte:
+
+Vamos desenvolver, usando apenas o React (Redux ainda não, ok!?) o app TrybeTube, em que desenvolveremos front que controla os vídeos a serem exibidos.
+
+_Protótipo_
+
+![TrybeTube](./prototipo.png)
+
+Desenvolveremos um TrybeTube, que possui os vídeos da Trybe! Não se preocupem, ainda não queremos que os vídeos sejam reproduzidos ainda...  =)
+A idéia aqui é que tenhamos uma funcionalidade básica assim como o YouTube.
+
+# Passo 1 - Começando um novo App #
+Lembram como criar um app novo? Mãos a obra!
+
+# Passo 2 - Criando um componente simples #
+Pensando no protótipo, como podemos dividí-lo em componentes? Lembre-se que ainda não usamos o redux.
+
+# Passo 3 - Definindo informações dinâmicas da aplicação #
+Quais estados são importantes aqui? Pq?
+
+# Passo 4 - Como funciona o App, com o que da página interagimos? #
+Abra a página do YouTube, dê uma olhada nas suas funcionalidades, como podemos reproduzi-la no nosso app? Será que podemos usar o react-router?
---
+++ exercises/M2/catchup/trybetube/src/data.js
@@ -0,0 +1,99 @@
+const videos = [
+  {
+    titulo: 'Aprendendo a dirigir com hamaji',
+    imagem: 'https://d3lp4xedbqa8a5.cloudfront.net/s3/digital-cougar-assets/whichcar/2020/07/07/6936/car_crash_golf_main.jpg',
+    publicado: '4/2/2021',
+    publicado_por: 'hamaji',
+    comentarios: [
+      {
+        nome: 'murilo',
+        conteudo: 'muito devagar, meio barbeiro...'
+      },
+      {
+        nome: 'nato',
+        conteudo: 'achei sensacional'
+      }
+    ]
+  },
+  {
+    titulo: 'Clean code do Roque',
+    imagem: 'https://p0.piqsels.com/preview/367/50/448/gloves-cleaning-clean-wash.jpg',
+    publicado: '01/02/2021',
+    publicado_por: 'murilo',
+    comentarios: [
+      {
+        nome: 'hamaji',
+        conteudo: 'que lecal!'
+      },
+      {
+        nome: 'ricci',
+        conteudo: 'até assustei com tanto conteúdo'
+      },
+      {
+        nome: 'dan',
+        conteudo: 'rock? onde?'
+      }
+    ]
+  },
+  {
+    titulo: 'Elvis não morreu',
+    imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu7CKJ4raMFCOwRGaLvFIk3F_HYJEy9p-7zQ&usqp=CAU',
+    publicado: '20/02/2021',
+    publicado_por: 'dan',
+    comentarios: [
+      {
+        nome: 'hamaji',
+        conteudo: 'aliens'
+      },
+      {
+        nome: 'nato',
+        conteudo: 'morreu msm não'
+      },
+      {
+        nome: 'ricci',
+        conteudo: 'já até vi ele, lá do lado de varginha...'
+      }
+    ]
+  },
+  {
+    titulo: 'Meu cachorro fala!',
+    imagem: 'https://cdn.dicionariopopular.com/imagens/doge-meme-og.jpg',
+    publicado: '17/02/2021',
+    publicado_por: 'oliva',
+    comentarios: []
+  },
+  {
+    titulo: 'Piscina de slime',
+    imagem: 'https://cdn.vox-cdn.com/thumbor/ziQOHE46YzZkdaLeIYcYScFsgWA=/0x0:1280x720/1200x800/filters:focal(521x312:725x516)/cdn.vox-cdn.com/uploads/chorus_image/image/65098899/dragon_quest_slimes.0.0.jpg',
+    publicado: '20/02/2021',
+    publicado_por: 'karine',
+    comentarios: [
+      {
+        nome: 'nato',
+        conteudo: 'oi?'
+      },
+      {
+        nome: 'hamaji',
+        conteudo: 'noooooooooossaaaaaaa'
+      }
+    ]
+  },
+  {
+    titulo: 'O et de varginha existe',
+    imagem: 'https://aventurasnahistoria.uol.com.br/media/uploads/ufo_sci-fi/et_de_varginha.jpg',
+    publicado: '01/01/2021',
+    publicado_por: 'hamaji',
+    comentarios: [
+      {
+        nome: 'nato',
+        conteudo: 'já veio aqui em casa...'
+      },
+      {
+        nome: 'karine',
+        conteudo: 'esse rosto é familiar'
+      }
+    ]
+  }
+];
+
+export default videos;
