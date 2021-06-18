9b6d4a0442bba84f6c0130e24ce5c68ae8debee9
brenno-calado <brenno.calado@gmail.com>
2021-06-18 15:23:46 -0300

    local modules

---
+++ exercises/M3/26.1/brlValue.js
@@ -0,0 +1,9 @@
+const brl = 5.37;
+
+const USDtoBRL = (usd) => usd * brl;
+
+module.exports = {
+  brl,
+  USDtoBRL,
+};
+
---
+++ exercises/M3/26.1/index.js
@@ -0,0 +1,6 @@
+const { brl, USDtoBRL } = require('./brlValue');
+
+const usd = 10;
+const converted = USDtoBRL(usd);
+
+console.log(brl, converted);
