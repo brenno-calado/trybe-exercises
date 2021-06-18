fba76caf9f5691c5caa2ec3d52641101c82f5f83
brenno-calado <brenno.calado@gmail.com>
2021-06-18 20:40:55 -0300

    desafio 5

--- exercises/M3/26.1/my-scripts/imc.js
+++ exercises/M3/26.1/my-scripts/imc.js
@@ -1,8 +1,34 @@
 const readline = require("readline-sync");
 
-const peso = readline.questionFloat("Digite seu peso(kg): ");
-const altura = readline.questionFloat("Digite sua altura(m): ");
+function calcIMC() {
+  const peso = readline.questionFloat("Digite seu peso(kg): ");
+  const altura = readline.questionFloat("Digite sua altura(m): ");
 
-const imc = Math.round((peso / Math.pow(altura, 2)) * 100) / 100;
+  const imc = Math.round((peso / Math.pow(altura, 2)) * 100) / 100;
 
-console.log(imc);
+  console.log(`IMC: ${imc}`)
+
+  if (imc < 18.5) {
+    return console.log(`Abaixo do peso (magreza)`);
+  }
+
+  if (imc >= 18.5 && imc < 25) {
+    return console.log(`Peso normal`);
+  }
+
+  if (imc >= 25 && imc < 30) {
+    return console.log(`Acima do peso (sobrepeso)`);
+  }
+
+  if (imc >= 30 && imc < 35) {
+    return console.log(`Obesidade grau I`);
+  }
+
+  if (imc >= 35 && imc < 40) {
+    return console.log(`Obesidade grau II`);
+  }
+
+  return console.log(`Obesidade grau III e IV`);
+}
+
+calcIMC();
