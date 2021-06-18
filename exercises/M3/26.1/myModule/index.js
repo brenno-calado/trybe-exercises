ea36f13d8e8d93b78873d704bf0cb83ae3f240ce
brenno-calado <brenno.calado@gmail.com>
2021-06-18 16:33:51 -0300

    customized hello

--- exercises/M3/26.1/myModule/index.js
+++ exercises/M3/26.1/myModule/index.js
@@ -1,5 +1,9 @@
 const brlValue = require('./brlValue');
 const usdValue = require('./usdValue');
+const readline = require('readline-sync');
+
+const name = readline.question('What is your name?');
+const age = readline.questionInt('What is your age?');
 
 module.exports = { brlValue, usdValue };
-console.log("Hello World!");
+console.log(`Hello ${name}! You are ${age} years old`);
