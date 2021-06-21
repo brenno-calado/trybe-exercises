0b294f95b632c6811a576fa9b266cc773ff79b65
brenno-calado <brenno.calado@gmail.com>
2021-06-21 19:18:31 -0300

    async com promise

--- exercises/M3/26.2/index.js
+++ exercises/M3/26.2/index.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 
-fs.readFile('./arquivo.txt', (err, content) => {
-  if (err) {
-    console.error(`Erro ao ler o arquivo: ${err.message}`);
-    return;
-  }
+function readFilePromise(fileName) {
+  return new Promise((resolve, reject) =>{
+    fs.readFile(fileName, (err, content) => {
+      if (err) return reject(err);
+      resolve(content);
+    });
+  });
+}
 
-  console.log(`Arquivo lido com sucesso. Conteúdo: ${content.toString('utf8')}`);
-});
+readFilePromise('./arquivo.txt')
+  .then(content => console.log(`Arquivo lido com sucesso.Conteúdo: ${content.toString('utf8')}`))
+  .catch(err => console.error(`Erro ao ler o arquivo: ${err.message}`));
 
