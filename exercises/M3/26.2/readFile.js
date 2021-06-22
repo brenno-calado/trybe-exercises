c4810300e5a14758758e13636a93a05df75d262b
brenno-calado <brenno.calado@gmail.com>
2021-06-22 11:34:20 -0300

    read async with promise

---
+++ exercises/M3/26.2/readFile.js
@@ -0,0 +1,13 @@
+const fs = require('fs').promises;
+
+const nomeDoArquivo = 'arquivo2.txt';
+
+fs.readFile(nomeDoArquivo, 'utf8')
+  .then((data) => {
+    console.log(`Conteúdo do arquivo: ${data}`);
+  })
+  .catch((err) => {
+    console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
+    process.exit(1);
+  });
+
