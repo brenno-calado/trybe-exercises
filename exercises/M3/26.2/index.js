8118ea539abfcb57f9757b4c4c0511e0a0edf880
brenno-calado <brenno.calado@gmail.com>
2021-06-21 21:53:25 -0300

    multiple promises

--- exercises/M3/26.2/index.js
+++ exercises/M3/26.2/index.js
@@ -10,6 +10,19 @@ function readFilePromise(fileName) {
 }
 
 readFilePromise('./arquivo.txt')
-  .then(content => console.log(`Arquivo lido com sucesso.Conteúdo: ${content.toString('utf8')}`))
+  .then(content => {
+    console.log(`Conteúdo: ${content.toString('utf8')}`);
+    console.log(`Tamanho: ${content.byteLength} bytes \n\n\n`);
+    return readFilePromise('./arquivo2.txt');
+  })
+  .then(secondContent => {
+    console.log(`Conteúdo ${secondContent.toString('utf8')}`);
+    console.log(`Tamanho: ${secondContent.byteLength} bytes \n\n\n`);
+    return readFilePromise('./arquivo3.txt');
+  })
+  .then(thirdContent => {
+    console.log(`Conteúdo ${thirdContent.toString('utf8')}`);
+    console.log(`Tamanho: ${thirdContent.byteLength} bytes \n\n\n`);
+  })
   .catch(err => console.error(`Erro ao ler o arquivo: ${err.message}`));
 
