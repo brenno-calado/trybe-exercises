dabde5182393343ad86644c70c0962497703f3c3
brenno-calado <brenno.calado@gmail.com>
2021-06-22 11:51:21 -0300

    promise all

---
+++ exercises/M3/26.2/promiseAll.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises;
+
+Promise.all([
+  fs.readFile('arquivo.txt'),
+  fs.readFile('arquivo2.txt'),
+  fs.readFile('arquivo3.txt'),
+])
+  .then(([file1, file2, file3]) => {
+    const fileSizeSum = file1.byteLength + file2.byteLength + file3.byteLength;
+    console.log(`Lidos 3 arquivos totalizando ${fileSizeSum} bytes`);
+  })
+  .catch((err) => {
+    console.error(`Erro ao ler arquivos: ${err.message}`);
+  });
+
