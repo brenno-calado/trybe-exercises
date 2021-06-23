04de8c4774079f15a5499c5fe2265bfa4d8b9b89
brenno-calado <brenno.calado@gmail.com>
2021-06-23 15:24:34 -0300

    hello world at port 3000

---
+++ exercises/M3/26.4/hello-express/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', handleHelloWorldRequest);
+
+app.listen(3000, () => {
+  console.log('Aplicação ouvindo na porta 3000')
+});
+
+function handleHelloWorldRequest(req, res) {
+  res.status(200).send('Hello World!');
+}
+
