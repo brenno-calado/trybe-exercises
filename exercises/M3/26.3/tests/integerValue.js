3fc3a65bf7d859ac83a76707277c187e67b53f8f
brenno-calado <brenno.calado@gmail.com>
2021-06-23 09:58:33 -0300

    taskOne

---
+++ exercises/M3/26.3/tests/integerValue.js
@@ -0,0 +1,23 @@
+const { expect } = require("chai");
+const { integerValue } = require("../integerValue");
+
+describe("A função integerValue retorna", () => {
+  it("o texto 'positivo' quando o parâmetro for maior que 0", () => {
+    const positivo = integerValue(6);
+    expect(positivo).to.be.a("string");
+    expect(positivo).to.be.equals("positivo");
+  });
+
+  it("o texto 'negativo' quando o parâmetro for menor que 0", () => {
+    const negativo = integerValue(-6);
+    expect(negativo).to.be.a("string");
+    expect(negativo).to.be.equals("negativo");
+  });
+
+  it("o texto 'neutro' quando o parâmetro for 0", () => {
+    const neutro = integerValue(0);
+    expect(neutro).to.be.a("string");
+    expect(neutro).to.be.equals("neutro");
+  });
+});
+
