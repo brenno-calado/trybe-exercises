e460837987421cfdbe087a150b9acb3dfb674db7
brenno-calado <brenno.calado@gmail.com>
2021-07-06 20:28:37 -0300

    integrate movieController to test

--- exercises/M3/27.4/tests/controllers/movieController.test.js
+++ exercises/M3/27.4/tests/controllers/movieController.test.js
@@ -1,9 +1,8 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 
-const MoviesController = {
-  create: () => {}
-};
+const MoviesService = require('../../services/movieService');
+const MoviesController = require('../../controllers/movieController');
 
 describe('Ao chamar o controller de create', () => {
   describe('quando o payload informado não é válido', () => {
@@ -17,7 +16,12 @@ describe('Ao chamar o controller de create', () => {
         .returns(response);
       response.send = sinon.stub()
         .returns();
-    })
+        sinon.stub(MoviesService, 'create').resolves(false);
+    });
+
+    after(() => {
+      MoviesService.create.restore();
+    });
 
     it('é chamado o status com o código 400', async () => {
       await MoviesController.create(request, response);
@@ -48,7 +52,12 @@ describe('Ao chamar o controller de create', () => {
         .returns(response);
       response.send = sinon.stub()
         .returns();
-    })
+      sinon.stub(MoviesService, 'create').resolves(true);
+    });
+
+    after(() => {
+      MoviesService.create.restore();
+    });
 
     it('é chamado o status com o código 201', async () => {
       await MoviesController.create(request, response);
