271144fae48652b9782fa9327b21ddae90f9f01f
brenno-calado <brenno.calado@gmail.com>
2021-07-12 16:02:02 -0300

    base64example

---
+++ exercises/M3/28.1/base64Example.js
@@ -0,0 +1,20 @@
+import { hmac } from 'bibliotecaDeHmac';
+
+function base64 (string) {
+  return Buffer.from(string).toString('base64');
+}
+
+const header = JSON.stringify({
+  alg: 'HS256',
+  type: 'JWT'});
+
+const payload = JSON.stringify({
+  sub: '1234567890',
+  name: 'John Doe',
+  admin: true});
+
+const secret = 'MinhaSenhaMuitoComplexa123';
+
+const assinatura = hmac(`${base64(header)}.${base64(payload)}`, secret);
+
+const token = `${base64(header)}.${base64(payload)}.${base64(assinatura)}`;
