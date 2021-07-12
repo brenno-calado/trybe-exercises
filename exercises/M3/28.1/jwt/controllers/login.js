a9ea218dce10cd399cdd21a2b0539af11039ded7
brenno-calado <brenno.calado@gmail.com>
2021-07-12 16:30:24 -0300

    jwt

--- exercises/M3/28.1/jwt/controllers/login.js
+++ exercises/M3/28.1/jwt/controllers/login.js
@@ -1,4 +1,7 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const secret = 'yoursecrettoken';
 
 module.exports = async (req, res) => {
   try {
@@ -11,7 +14,14 @@ module.exports = async (req, res) => {
 
   if (!user || user.password !== password) return res.status(401).json({ message: 'Usuário não existe ou senha inválida' });
 
-  return res.status(200).json({ message: 'Login efetuado com sucesso'});
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'H256',
+  }
+
+  const token = jwt.sign({ data: user }, secret, jwtConfig);
+
+  return res.status(200).json({ token });
   } catch (e) {
     return res.status(500).json({ message: 'Erro interno', error: e });
   }
