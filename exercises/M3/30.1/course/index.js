764dbac13b87c603af634104060582d2800d2cf1
brenno-calado <brenno.calado@gmail.com>
2021-07-26 15:53:04 -0300

    single responsibility principle

--- exercises/M3/30.1/course/index.js
+++ exercises/M3/30.1/course/index.js
@@ -1,49 +1,50 @@
+// ./index.js
+
+/* "Converter" */
+const percentageGradesIntoLetters = ({ name, disciplines }) => ({
+  name,
+  disciplines: disciplines.map(({ name, grade }) => {
+    let letterGrade;
+
+    if (grade >= 0.9) letterGrade = 'A';
+    else if (grade >= 0.8) letterGrade = 'B';
+    else if (grade >= 0.7) letterGrade = 'C';
+    else if (grade >= 0.6) letterGrade = 'D';
+    else if (grade >= 0.1) letterGrade = 'E';
+    else letterGrade = 'F';
+
+    return { name, grade, letterGrade };
+  })});
+
+/* "Determinar" */
+const approvedStudents = ({ disciplines }) =>
+  disciplines.every(({ grade }) => grade > 0.7);
+
+/* "Atualizar" */
+const updateApprovalData = ({ name: studentName, disciplines }) => {
+  console.log(`A pessoa com nome ${studentName} foi aprovada!`);
+
+  disciplines.map(({ name, letterGrade }) =>
+    console.log(`${name}: ${letterGrade}`));
+};
+
 function setApproved(students) {
-  const studentsWithLetterGrade = students.map((student) => {
-    const disciplinesWithLetterGrade = student.disciplines.map((discipline) => {
-      if (discipline.grade >= 0.9) discipline.letterGrade = 'A';
-      else if (discipline.grade >= 0.8) discipline.letterGrade = 'B';
-      else if (discipline.grade >= 0.7) discipline.letterGrade = 'C';
-      else if (discipline.grade >= 0.6) discipline.letterGrade = 'D';
-      else if (discipline.grade >= 0.1) discipline.letterGrade = 'E';
-      else discipline.letterGrade = 'F';
-
-      return discipline;
-    });
-
-    return {
-      name: student.name,
-      disciplines: disciplinesWithLetterGrade,
-    };
-  });
-
-  const approvedStudents = studentsWithLetterGrade.filter(({ disciplines }) =>
-    disciplines.every((discipline) => discipline.grade > 0.7));
-
-  /* Finja que o console.log é algo que atualiza uma base de dados */
-  approvedStudents.map(({ name, disciplines }) => {
-    console.log(`A pessoa com nome ${name} foi aprovada!`);
-    disciplines.map(({ name, letterGrade }) =>
-      console.log(`${name}: ${letterGrade}`));
-  });
+  students
+    .map(percentageGradesIntoLetters)
+    .filter(approvedStudents)
+    .map(updateApprovalData);
 }
 
-/* Abaixo temos um exemplo de execução */
-const students = [
-  {
-    name: 'Lee',
-    disciplines: [
-      { name: 'matemática', grade: 0.8 },
-      { name: 'história', grade: 0.6 },
-    ],
-  },
-  {
-    name: 'Clementine',
-    disciplines: [
-      { name: 'matemática', grade: 0.8 },
-      { name: 'história', grade: 0.9 },
-    ],
-  },
-];
+/* Exemplo de execução */
+// ...
 
-setApproved(students);
+/*
+  Não se esqueça que é necessário exportar ao final as
+  funções para que você possa testa-las de forma unitária
+*/
+module.exports = {
+  percentageGradesIntoLetters,
+  approvedStudents,
+  updateApprovalData,
+  setApproved,
+};
