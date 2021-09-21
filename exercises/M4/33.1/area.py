a1757465e53a12358804fec66824b58b384b8d0d
brenno-calado <brenno.calado@gmail.com>
2021-09-21 15:50:43 -0300

    add area module

---
+++ exercises/M4/33.1/area.py
@@ -0,0 +1,15 @@
+PI = 3.14
+
+def square(side):
+    '''Calculate area of square.'''
+    return side * side
+
+
+def rectangle(length, width):
+    '''Calculate area of rectangle.'''
+    return length * width
+
+
+def circle(radius):
+    '''Calculate area of circle.'''
+    return PI * radius * radius
