dc2fd1b11d8db9af03b4750ef241291d221000a2
brenno-calado <brenno.calado@gmail.com>
2021-10-14 15:26:06 -0300

    34.2

---
+++ exercises/M4/34.2/sale_report.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+import json
+
+
+class SalesReport(ABC):
+    def __init__(self, export_file):
+        self.export_file = export_file
+
+    def build(self):
+        return [{
+                'Coluna 1': 'Dado 1',
+                'Coluna 2': 'Dado 2',
+                'Coluna 3': 'Dado 3'
+                },
+                {
+                'Coluna 1': 'Dado A',
+                'Coluna 2': 'Dado B',
+                'Coluna 3': 'Dado C'
+                }]
+
+    @abstractmethod
+    def serialize(self):
+        raise NotImplementedError
+
+
+class SalesReportJSON(SalesReport):
+    def serialize(self):
+        with open(self.export_file + '.json', 'w') as file:
+            json.dump(self.build(), file)
